59211e69b7252ec54922c3b8d600b1ec11c7483d
wemendez2801 <wemendez.20@est.ucab.edu.ve>
2024-02-19 18:04:05 -0400

    Navbar
    Navbar terminada

---
+++ proyecto-labs/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent} from './navbar/navbar.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,CommonModule,NavbarComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'proyecto-labs';
+}
