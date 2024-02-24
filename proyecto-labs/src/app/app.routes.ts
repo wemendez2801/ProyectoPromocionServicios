bf7d89d5cd3e5ca82615c5aea8bdaddad20f80b7
wemendez2801 <wemendez.20@est.ucab.edu.ve>
2024-02-24 13:52:57 -0400

    Arreglo bug Landing
    Se evita duplicacion de contenido

--- proyecto-labs/src/app/app.routes.ts
+++ proyecto-labs/src/app/app.routes.ts
@@ -2,5 +2,8 @@ import { Routes } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
 
 export const routes: Routes = [
-    
+   /*  
+        path: '',
+        component: LandingComponent
+     */
 ];
