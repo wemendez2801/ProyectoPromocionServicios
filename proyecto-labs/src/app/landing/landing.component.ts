414c7d5c9dc6065cda1494e4b72638fc5e396601
wemendez2801 <wemendez.20@est.ucab.edu.ve>
2024-02-26 11:16:18 -0400

    Cambios Landing
    Arreglos de Footer y secciones

--- proyecto-labs/src/app/landing/landing.component.ts
+++ proyecto-labs/src/app/landing/landing.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../navbar/navbar.component';
+import {FooterComponent} from '../footer/footer.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, NavbarComponent],
+  imports: [RouterOutlet, CommonModule, NavbarComponent, FooterComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
